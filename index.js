da81a31c08e338b1efd13ecd7f0b3e81cfbbaea1
BAGGIO Matteo <matteo.baggio@soprasteria.com>
2017-11-09 16:36:53 +0100

    Fix export issue

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import FirebaseCollection from "./lib/FirebaseCollection";
 import FirebaseModel from "./lib/FirebaseModel";
 import sync from "./lib/utils/sync";
 
-export FirebaseCollection;
-export FirebaseModel;
-export sync;
+export {
+	FirebaseCollection as FirebaseCollection,
+	FirebaseModel as FirebaseModel,
+	sync as sync
+}
